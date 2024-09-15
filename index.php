28f600737910eaa5386171cad50b3826353118fb
CoatlX <coatlex@gmail.com>
2024-09-15 10:20:57 -0600

    Add process.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+echo '<h1> Estamos recibiendo lo siguiente'</h1>';
+echo '<pre>';'
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primera Parte de PHP</title>
+ </head>
+ <body>
+    <h3>Registro de equipo</h3>
+    <form action="process.php" method="""></form>
+    <label for="gamertag"> Gamertag</label>
+ </body>
+ </html>
